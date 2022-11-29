80a7c3fb539aaed0ff52adae16ce4d1da1f77eb5
Maruf_Monem <marufmonem@gmail.com>
2022-11-29 10:50:11 +0600

    Refactoring code

--- src/test/java/NetworkLoadingFailed.java
+++ src/test/java/NetworkLoadingFailed.java
@@ -20,11 +20,8 @@ public class NetworkLoadingFailed {
 //        manipulating network items
         devTools.send(Network.emulateNetworkConditions(true, 3000,20000, 100000, Optional.of(ConnectionType.CELLULAR2G)));
 //        parameters
-//        1: network would be active
-//        2. There would be a 3s delay in fetching results
-//        3. The speed of download: 20000 bytes/sec
-//        3. The speed of upload: 100000 bytes/sec
-//        4. Optional field: network speed 2g
+//        1: network would not be active
+
 
 //        Failed to load the website
         devTools.addListener(Network.loadingFailed(), loadingFailed -> {
@@ -32,16 +29,9 @@ public class NetworkLoadingFailed {
             System.out.println( loadingFailed.getTimestamp());
 
         });
-
-//        capturing the time
-        long startTime = System.currentTimeMillis();
         driver.get("https://rahulshettyacademy.com/angularAppdemo/");
         driver.findElement(By.cssSelector("button[routerlink*='library']")).click();
 
-//        end time
-        long endTime = System.currentTimeMillis();
-        System.out.println("In 2g: The whole process took: 14599");
-        System.out.println("The whole process took: " + (endTime-startTime));
-        System.out.println("Without any network controls: The whole process took: 570");
+
     }
 }
